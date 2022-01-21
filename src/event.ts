c5216a9bdadd01d7764c0ae3cbbc6ff4402bfac6
jayyu <jayyu@123u.com>
2022-01-21 21:59:36 +0800

    initial commit

---
+++ src/event.ts
@@ -0,0 +1,3 @@
+export default class Event {
+    public static readonly LAYA_COMPILE = "vsc-plugin.laya.compile";
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import Event from './event';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vsc-plugin" is now active!');
@@ -13,14 +14,20 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vsc-plugin.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand(Event.LAYA_COMPILE, () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vsc_plugin!');
+		// vscode.window.showInformationMessage('插件已启动！');
+		let options: vscode.TerminalOptions = {
+			name: 'laya编译',
+			cwd: vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders[0].uri.fsPath : '',
+		}
+		let terminal = vscode.window.createTerminal(options);
+
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
